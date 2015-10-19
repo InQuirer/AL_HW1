d21c96720b4f65fe5ae1cf465abaca9ff9383f89
Habib Mohammad <vova313@gmail.com>
2015-10-19 16:42:41 +0300

    uploading AL04

---
+++ AL04/src/dataStructures/Node.java
@@ -0,0 +1,11 @@
+package dataStructures;
+
+public class Node {
+    public String name;
+    public float k;
+    
+    public Node(String name, float k) {
+    	this.name = name;
+    	this.k = k;
+    }
+}
